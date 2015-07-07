884ee88f6cca12046b5c1039e4e8af3a84360878
Hackbright Student <info@hackbrightacademy.com>
2015-07-06 17:04:51 -0700

    Initial commit, word count already complete

---
+++ wordcount.py
@@ -0,0 +1,28 @@
+def count_words(file_name):
+    """
+        Input: file name - string with file path name 
+        Output: Print to stdout the words in the file and the number of times each one occurs
+
+        count_words() iterates over the words in a file and counts how many time each word 
+        occurs. The function then prints this info to stdout. The function returns None.
+    """
+    #Open a file
+    file = open(file_name)
+
+    word_count = {}
+
+    #Count how many times each space-separated word occurs in the file
+    for line in file:
+        words = line.split(" ")
+        for word in words:
+            # If the word is already in word_count, increment count
+            # If the word is not in word_count, initialize the key value pair with value of 1
+            word_count[word.rstrip()] = word_count.get(word, 0) + 1
+
+    #Print word counts to stdout
+    for w, n in word_count.items():
+        print "%s %d" % (w, n)
+
+
+
+count_words('test.txt')
